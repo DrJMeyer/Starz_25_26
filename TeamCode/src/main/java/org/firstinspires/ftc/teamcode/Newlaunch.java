74cc4fd1f51bc6e07110680015d9dede6305de79
sertyui <bstafford@lakehillprep.org>
2025-11-05 11:33:54 -0600

    This is the code im fleshing out

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Newlaunch.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+@TeleOp(name=Newlaunch, group = "first basis")
+private int apriltage
+public class Newlaunch {
+}
