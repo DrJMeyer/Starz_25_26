fae5a03baad258b1b288129d2dcc5b73b5be0df2
willi <williamshah@lakehillprep.org>
2025-11-06 12:49:59 -0600

    webcam now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/activation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/activation.java
@@ -7,97 +7,43 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.Servo;
+import android.annotation.SuppressLint;
 
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
 
 @TeleOp
 public class activation extends LinearOpMode {
-
-    // Initialize all of our variables here
     private DcMotor frontLeftDrive = null;
     private DcMotor backLeftDrive = null;
     private DcMotor frontRightDrive = null;
     private DcMotor backRightDrive = null;
-    private Servo sup;
-    private NormalizedColorSensor colorSensor ;
+    private DcMotor intake = null;
+    private DcMotor motorRight;
+    private DcMotor motorLeft;
+    private Servo megabeam;
+    private Servo launchdoor;
+    private NormalizedColorSensor colorSensor;
     private static final boolean USE_WEBCAM = true;
     private AprilTagProcessor aprilTag;
     private VisionPortal visionPortal;
 
 
-
-
-    // The main program begins here.
-    ///// runOpMode() is called at the beginning of the LinearOpMode class we are running
-    @Override
-    public void runOpMode() {
-
-        // Initialize all of our hardware variables by connecting script variables
-        // with hardware configuration established on the control hub.
-
-        // 4 driving motors
-        frontLeftDrive = hardwareMap.get(DcMotor.class, "fpd");
-        backLeftDrive = hardwareMap.get(DcMotor.class, "bpd");
-        frontRightDrive = hardwareMap.get(DcMotor.class, "fsd");
-        backRightDrive = hardwareMap.get(DcMotor.class, "bsd");
-
-        // a servo. Which one?
-        sup = hardwareMap.get(Servo.class, "sup");
-
-        // color sensor (inside circular magazine)
-        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "color");
-
-        // Still need:
-        //// 1 more servo ( 2 total: one to open the launch flap and one to spin the mag)
-        //// 1 motor - spin the intake stars
-        //// 2 motors - launch motors
-        //// 1 webcam
-
-        // Set direction of drive motors
-        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
-        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
-        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
-        backRightDrive.setDirection(DcMotor.Direction.REVERSE);
-
-        // This is where we want to read the april tag and determine ball order.
-        ///// readAprilID()
-        ///// create an array (like a list) of the order you need to match for balls
-        // you will hold onto that array and reference it later in code which will decide which ball to shoot.
-
-        // Send note to driver hub that everything looks good.
-        telemetry.addData("Status", "Intialized");
-        telemetry.update();
-
-        // Program hangs out until play button is pressed
-        waitForStart();
-
-        // Robot will continually move through this list of code until stop button is pressed on driver hub
-        while (opModeIsActive()) {
-
-            // First code checks if any gamepad is inputting a motion control (joystick activation)
-            moverobot();
-
-            // Intake code. Also activated by the press of a button and with a deactivation mechanism.
-            //// This may also need to include a method of rotating the magazine as each ball is gathered.
-
-            // You will want a color sort and a launch code here. Likely connected functions activated by the press of a button
-            checkcolor();
-        }
-    }
-
-
-    private  void moverobot(){
-
-        // This function looks great. Should be ready to test
+    private void moverobot() {
         double axial = gamepad1.left_stick_y;
         double lateral = gamepad1.left_stick_x;
         double yaw = gamepad1.right_stick_x;
-        double frontLeftPower  = axial + lateral + yaw;
+        double frontLeftPower = axial + lateral + yaw;
         double frontRightPower = axial - lateral - yaw;
-        double backLeftPower   = axial - lateral + yaw;
-        double backRightPower  = axial + lateral - yaw;
+        double backLeftPower = axial - lateral + yaw;
+        double backRightPower = axial + lateral - yaw;
         frontLeftDrive.setPower(frontLeftPower);
         frontRightDrive.setPower(frontRightPower);
         backLeftDrive.setPower(backLeftPower);
@@ -107,9 +53,7 @@ public class activation extends LinearOpMode {
         telemetry.update();
     }
 
-    private void checkcolor(){
-
-        // Check in with Brady on this code. It will be merged with his.
+    private void checkcolor() {
         telemetry.addData("Gain", 16);
         colorSensor.setGain(16);
         NormalizedRGBA colors = colorSensor.getNormalizedColors();
@@ -119,10 +63,87 @@ public class activation extends LinearOpMode {
                 .addData("Blue", "%.3f", colors.blue);
     }
 
-    // private void intakeBall(){
-    //  code here
-    // }
 
+    @Override
+    public void runOpMode() {
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "fpd");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "bpd");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "fsd");
+        backRightDrive = hardwareMap.get(DcMotor.class, "bsd");
+        megabeam = hardwareMap.get(Servo.class, "megabeam");
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "color");
+
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        initAprilTag();
+
+        int detectedTagID = -1;
+        telemetry.addData("Status", "Intialized");
+        telemetry.update();
+
+        waitForStart();
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                moverobot();
+                checkcolor();
+                telemetryAprilTag();
+
+
+
+
+                telemetry.update();
+
+                if (gamepad1.dpad_down) {
+                    visionPortal.stopStreaming();
+                } else if (gamepad1.dpad_up) {
+                    visionPortal.resumeStreaming();
+                }
+
+                sleep(20);
+
+            }
+            visionPortal.close();
+
+
+        }
+    }
+
+    private void initAprilTag() {
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
+        if (USE_WEBCAM) {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    hardwareMap.get(WebcamName.class, "camera"), aprilTag);
+
+
+        } else {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    BuiltinCameraDirection.BACK, aprilTag);
+
+
+        }
+    }
+
+
+    private void telemetryAprilTag() {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+
+    }
 }
-
-
