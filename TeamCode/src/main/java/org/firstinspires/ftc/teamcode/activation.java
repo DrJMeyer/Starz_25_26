f9b6d297480573a45dd76a0f6ec491a49227f2f8
DrMeyer <jmeyer@lakehillprep.org>
2025-11-06 20:59:01 -0600

    Cleaned up code a bit and combined Brady and Will's work.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/activation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/activation.java
@@ -19,15 +19,25 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
 
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// In the future work on the main runOPmode() function in this class
+// but any additional functions should be drafted in their own separate class
+// and then copy pasted in here once you know you have the latest version of the code.
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 @TeleOp
 public class activation extends LinearOpMode {
+
+    // Initialize all of our variables here
     private DcMotor frontLeftDrive = null;
     private DcMotor backLeftDrive = null;
     private DcMotor frontRightDrive = null;
     private DcMotor backRightDrive = null;
+    private Servo sLaunch;
+    private Servo sIntake;
     private DcMotor intake = null;
-    private DcMotor motorRight;
-    private DcMotor motorLeft;
+    private DcMotor lLauncher;
+    private DcMotor rLauncher;
     private Servo megabeam;
     private Servo launchdoor;
     private NormalizedColorSensor colorSensor;
@@ -35,8 +45,109 @@ public class activation extends LinearOpMode {
     private AprilTagProcessor aprilTag;
     private VisionPortal visionPortal;
 
+    private int[] motif;
+    public int IDnum;
 
+
+    // The main program begins here.
+
+    /// // runOpMode() is called at the beginning of the LinearOpMode class we are running
+    @Override
+    public void runOpMode() {
+        // Initialize all of our hardware variables by connecting script variables
+        // with hardware configuration established on the control hub.
+
+        // 4 driving motors
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "fpd");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "bpd");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "fsd");
+        backRightDrive = hardwareMap.get(DcMotor.class, "bsd");
+
+        //launch motors and
+        lLauncher = hardwareMap.get(DcMotor.class, "lL");
+        rLauncher = hardwareMap.get(DcMotor.class, "rL");
+
+        // a servo. Which one?
+        sLaunch = hardwareMap.get(Servo.class, "sup");
+        sIntake = hardwareMap.get(Servo.class, "dog");
+
+        // color sensor (inside circular magazine)
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "color");
+
+        // Still need:
+        //// 1 webcam
+
+
+        // Set direction of drive motors
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // This is where we want to read the april tag and determine ball order.
+
+        IDnum = readAprilID();
+
+        ///// create an array (like a list) of the order you need to match for balls
+        // you will hold onto that array and reference it later in code which will decide which ball to shoot.
+        if (IDnum == 21)
+        {
+            motif[0]=0;
+            motif[1]=1;
+            motif[2]=1;
+        }
+        else if (IDnum == 22)
+        {
+            motif[0]=1;
+            motif[1]=0;
+            motif[2]=1;
+        }
+        else if (IDnum == 23)
+        {
+            motif[0]=1;
+            motif[1]=1;
+            motif[2]=0;
+        }
+
+        // Send note to driver hub that everything looks good.
+        telemetry.addData("Status", "Intialized");
+        telemetry.update();
+
+        // Program hangs out until play button is pressed
+        waitForStart();
+
+        // Robot will continually move through this list of code until stop button is pressed on driver hub
+        while (opModeIsActive()) {
+
+            // First code checks if any gamepad is inputting a motion control (joystick activation)
+            moverobot();
+
+            // Intake code. Also activated by the press of a button and with a deactivation mechanism.
+            //// This may also need to include a method of rotating the magazine as each ball is gathered.
+
+            // You will want a color sort and a launch code here. Likely connected functions activated by the press of a button
+            checkcolor();
+            launchCode(motif);
+
+
+
+            telemetry.update();
+
+            if (gamepad1.dpad_down) {
+                visionPortal.stopStreaming();
+            } else if (gamepad1.dpad_up) {
+                visionPortal.resumeStreaming();
+            }
+
+            sleep(20);
+
+        }
+        visionPortal.close();
+
+    }
     private void moverobot() {
+
+        // This function looks great. Should be ready to test
         double axial = gamepad1.left_stick_y;
         double lateral = gamepad1.left_stick_x;
         double yaw = gamepad1.right_stick_x;
@@ -54,6 +165,7 @@ public class activation extends LinearOpMode {
     }
 
     private void checkcolor() {
+        // Check in with Brady on this code. It will be merged with his.
         telemetry.addData("Gain", 16);
         colorSensor.setGain(16);
         NormalizedRGBA colors = colorSensor.getNormalizedColors();
@@ -63,52 +175,19 @@ public class activation extends LinearOpMode {
                 .addData("Blue", "%.3f", colors.blue);
     }
 
+    private void launchCode(int[] colorList)  {
+        if (gamepad1.left_trigger == 1.0) {
+            lLauncher.setPower(.5);
+            rLauncher.setPower(.5);
+            sLaunch.setPosition(0);
 
-    @Override
-    public void runOpMode() {
-        frontLeftDrive = hardwareMap.get(DcMotor.class, "fpd");
-        backLeftDrive = hardwareMap.get(DcMotor.class, "bpd");
-        frontRightDrive = hardwareMap.get(DcMotor.class, "fsd");
-        backRightDrive = hardwareMap.get(DcMotor.class, "bsd");
-        megabeam = hardwareMap.get(Servo.class, "megabeam");
-        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "color");
-
-        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
-        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
-        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
-        backRightDrive.setDirection(DcMotor.Direction.REVERSE);
-        initAprilTag();
-
-        int detectedTagID = -1;
-        telemetry.addData("Status", "Intialized");
-        telemetry.update();
-
-        waitForStart();
-        if (opModeIsActive()) {
-            while (opModeIsActive()) {
-                moverobot();
-                checkcolor();
-                telemetryAprilTag();
-
-
-
-
-                telemetry.update();
-
-                if (gamepad1.dpad_down) {
-                    visionPortal.stopStreaming();
-                } else if (gamepad1.dpad_up) {
-                    visionPortal.resumeStreaming();
-                }
-
-                sleep(20);
-
-            }
-            visionPortal.close();
-
+            // Loop through the motif list each time matching a ball in the magazine to the correct color.
+            checkcolor();
 
         }
     }
+
+
 
     private void initAprilTag() {
         aprilTag = AprilTagProcessor.easyCreateWithDefaults();
@@ -123,6 +202,23 @@ public class activation extends LinearOpMode {
 
 
         }
+    }
+
+    private int readAprilID() {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+
+            }
+
+
+            return detection.id;
+
+        }
+
+        return 0;
     }
 
 
