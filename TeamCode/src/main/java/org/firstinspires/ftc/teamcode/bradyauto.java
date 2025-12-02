7369896bf3e7699c2d633adb72710d10746cb04b
sertyui <bstafford@lakehillprep.org>
2025-12-02 13:03:55 -0600

    in progress

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bradyauto.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+ * This OpMode illustrates the concept of driving a path based on time.
+ * The code is structured as a LinearOpMode
+ *
+ * The code assumes that you do NOT have encoders on the wheels,
+ *   otherwise you would use: RobotAutoDriveByEncoder;
+ *
+ *   The desired path in this example is:
+ *   - Drive forward for 3 seconds
+ *   - Spin right for 1.3 seconds
+ *   - Drive Backward for 1 Second
+ *
+ *  The code is written in a simple form with no optimizations.
+ *  However, there are several ways that this type of sequence could be streamlined,
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="Robot: Auto Drive By Time", group="Robot")
+@Disabled
+public class RobotAutoDriveByTime_Linear extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private DcMotor frontLeftDrive = null;
+    private DcMotor backLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor backRightDrive = null;
+    private DcMotor lLauncher;
+    private DcMotor rLauncher;
+    private Servo sLaunch;
+    private Servo sIntake;
+    static final double     FORWARD_SPEED = 0.6;
+    @Override
+    public void runOpMode() {
+
+        // Initialize the drive system variablesfrontLeftDrive = hardwareMap.get(DcMotor.class, "fpd");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "fpd");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "bpd");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "fsd");
+        backRightDrive = hardwareMap.get(DcMotor.class, "bsd");
+        lLauncher = hardwareMap.get(DcMotor.class, "lL");
+        rLauncher = hardwareMap.get(DcMotor.class, "rL");
+        sLaunch = hardwareMap.get(Servo.class, "sup");
+        sIntake = hardwareMap.get(Servo.class, "dog");
+
+        waitForStart();
+
+        set
+}
