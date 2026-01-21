0f697e48eaf0192fcc47e0086491f79baefe1cfe
RaduUdr <Rudrea@lakehillprep.org>
2026-01-21 11:26:47 -0600

    meepmeep

---
+++ TeamCode/src/main/java/trajectorysequence/trajectorysequence/EmptySequenceException.java
@@ -0,0 +1,7 @@
+package trajectorysequence.trajectorysequence;
+
+public class EmptySequenceException extends RuntimeException {
+    public EmptySequenceException(String message) {
+        super(message);
+    }
+}
---
+++ TeamCode/src/main/java/trajectorysequence/trajectorysequence/TrajectorySequenceBuilder.java
@@ -0,0 +1,4 @@
+package trajectorysequence.trajectorysequence;
+
+public class TrajectorySequenceBuilder {
+}
