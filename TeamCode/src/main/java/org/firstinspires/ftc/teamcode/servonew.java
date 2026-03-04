c2713bca43a9dad8448f16fdfeac6918dda14c1f
alt <bstafford@lakehillprep.org>
2026-03-04 12:04:57 -0600

    g

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servonew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servonew.java
@@ -6,20 +6,28 @@ public class servonew extends LinearOpMode {
     private Servo servo1 = null;
     private Servo servo2 = null;
 
-    public void runOpMode(){
-        servo1= hardwareMap.get(Servo.class, "serv1");
+    public void runOpMode() {
+        servo1 = hardwareMap.get(Servo.class, "serv1");
         servo2 = hardwareMap.get(Servo.class, "serv1");
 
 
-while (opModeIsActive()){
+        while (opModeIsActive()) {
 
-    if(gamepad1.dpad_down){
+            if (gamepad1.dpad_down) {
+                servo1.setPosition(0);
+                servo2.setPosition(0);
+            }
+            if (gamepad1.dpad_up) {
+                servo1.setPosition(.8);
+                servo2.setPosition(.8);
+            }
+            if (gamepad1.dpad_right) {
+                servo1.setPosition(1);
+            }
+            if (gamepad1.dpad_left) {
+                servo2.setPosition(1);
+            }
 
+        }
     }
-
-
-}
-
-    }
-
 }
