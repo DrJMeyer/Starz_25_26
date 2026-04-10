91dde4b538af1eecba8f667952694d42a95bdc90
willi <williamshah@lakehillprep.org>
2026-04-10 10:34:58 -0500

    radu save me

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/id.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/id.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.annotation.SuppressLint;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -19,12 +22,19 @@ public class id extends LinearOpMode {
     private int [] motif;
     public int IDnum;
 
+    private DcMotor move;
+    private DcMotor motor;
+
     @Override
     public void runOpMode(){
+
+        move = hardwareMap.get(DcMotor.class, "move");
+        motor = hardwareMap.get(DcMotor.class, " motor");
 
         motif[0] = 21;
         motif[1] = 22;
         motif[2] = 23;
+        IDnum = 0;
 
         initAprilTag();
 
@@ -36,6 +46,41 @@ public class id extends LinearOpMode {
 
         if(opModeIsActive()){
             while (opModeIsActive()){
+                AprilTagDetection tag = null;
+                List<AprilTagDetection> detections = aprilTag.getDetections();
+
+                for (AprilTagDetection detection : detections) {
+                    if (detection.metadata != null) {
+                        tag = detection;
+                        break; // use first valid tag
+                    }
+                }
+
+
+
+                telemetryAprilTag();
+                telemetry.update();
+
+                if (IDnum == motif[0]){
+                    motor.setPower(1);
+                } else if (IDnum == motif[1]) {
+                    move.setPower(1);
+                } else if (IDnum == motif[2]) {
+                    move.setPower(1);
+                    motor.setPower(1);
+                } else {
+                    motor.setPower(0);
+                    move.setPower(0);
+                }
+
+                // camera controls
+                if (gamepad1.dpad_down) {
+                    visionPortal.stopStreaming();
+                } else if (gamepad1.dpad_up) {
+                    visionPortal.resumeStreaming();
+                }
+
+                sleep(20);
 
             }
         }
@@ -54,12 +99,14 @@ public class id extends LinearOpMode {
         }
     }
 
+    @SuppressLint("DefaultLocale")
     private void telemetryAprilTag() {
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
         telemetry.addData("# AprilTags Detected", currentDetections.size());
 
         for (AprilTagDetection detection : currentDetections) {
             if (detection.metadata != null) {
+                IDnum = detection.id;
                 telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
                 telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f (inch)",
                         detection.ftcPose.x,
